e72b955ecac46f01835fcddba9ef6bf3db414131
shiv1433 <109666214+shiv1433@users.noreply.github.com>
2022-08-26 10:28:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-    required_version = "~> 1.0.3" // added latest version
+    required_version = "~> 4.27.0" // added latest version
     required_providers {
     aws = {
       source  = "hashicorp/aws"
